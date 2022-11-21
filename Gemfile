4575b98378847ef286e960b6ea58e8a0b739f287
Chris Piazza <piazza.chris@gmail.com>
2022-11-21 15:43:37 -0500

    updates

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'pry'
-gem 'nokogiri'
+gem 'nokogiri', '~> 1.13', '>= 1.13.9'
 gem 'httparty', '~> 0.18.1'
--- scraper.rb
+++ scraper.rb
@@ -4,16 +4,22 @@ require 'nokogiri'
 require 'pry'
 
 ## Get all the archive links
-archive_html = open("https://www.humblerecipes.com/archives.html")
-archive_page = Nokogiri::HTML(archive_html)
+archive_page = Nokogiri::HTML(URI.open("https://www.humblerecipes.com/archives.html"))
+
 archive_links = archive_page.css('li.archive-list-item').map { |e| e.children.first.attributes["href"].value }
 
 ## Follow each archive link
 archive_links.each do |archive_link|
-  month_html = open(archive_link)
-  month_page = Nokogiri::HTML(month_html)
+  month_page = Nokogiri::HTML(URI.open(archive_link))
 
   ## Collect the links to each individual post
-  post_links = month_page.css('p.entry-more-link').map { |e| e.children.css('a').first.attributes["href"].value }
-  post_links.each { |l| puts l }
+  # post_links = month_page.css('p.entry-more-link').map { |e| e.children.css('a').first.attributes["href"].value }
+  post_links = month_page.css('h3.entry-header').map { |e| e.children.css('a').first.attributes["href"].value }
+
+  # post_links.each { |l| puts l }
+  #
+  puts post_links.first
+  # post_page = Nokogiri::HTML(URI.open(post_links.first))
+  # puts post_page
+
 end
